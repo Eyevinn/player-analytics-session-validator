8dff662d98324297a114a8a28fce14980114ada6
Oscar Nord <oscar.nord@gmail.com>
2021-12-27 08:31:58 +0100

    init project (#1)
    * chore: init project
    
    Co-Authored-By: Nicholas Frederiksen <nikko.frederiksen@gmail.com>
    
    * chore: add readme
    
    * chore: fix formatting
    
    * fix typo in test
    
    * chore: remove duplicated logger file and debug log in test
    
    * chore: update package.json before merge
    
    Co-authored-by: Nicholas Frederiksen <nikko.frederiksen@gmail.com>

---
+++ lib/EventDB.ts
@@ -0,0 +1,41 @@
+import winston from 'winston';
+
+export class EventDB {
+  logger: winston.Logger;
+  DBAdapter: any;
+
+  constructor(logger: winston.Logger) {
+    this.logger = logger;
+  }
+
+  /*
+   *
+   * @param {string} sessionId of a session
+   * @param {string} tableName the table to get the event from
+   *
+  */
+  async getEvents(sessionId: string, tableName: string): Promise<any[]> {
+    try {
+      if (!this.DBAdapter) {
+        let dbAdapter: any;
+        switch (process.env.DB_TYPE) {
+          case 'DYNAMODB':
+            dbAdapter = (await import('@eyevinn/player-analytics-shared')).DynamoDBAdapter;
+            break;
+          default:
+            this.logger.warn(`No database type specified`);
+            return [{ Message: 'No database type specified' }];
+        }
+        this.DBAdapter = new dbAdapter(this.logger);
+      }
+       const list = await this.DBAdapter.getItemsBySession({
+        tableName: tableName,
+        sessionId: sessionId,
+      });
+      return list;
+    } catch (err) {
+      this.logger.error(err);
+      return [];
+    }
+  }
+}
---
+++ logging/logger.ts
@@ -0,0 +1,45 @@
+import winston from 'winston';
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevEnv = env === 'development';
+  return isDevEnv ? 'debug' : 'info';
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const transports = [new winston.transports.Console()];
+
+const Logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+  silent: process.env.JEST_WORKER_ID !== undefined,
+});
+
+export default Logger;
